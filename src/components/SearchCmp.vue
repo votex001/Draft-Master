da04fdce81311ce88d7c2a8b51d66b34c1959320
votex001 <jeneea45@gmail.com>
2025-04-02 21:16:37 +0300

    search are ready

---
+++ src/components/SearchCmp.vue
@@ -0,0 +1,65 @@
+<template>
+  <section class="search">
+    <input
+      v-model="searchValue"
+      :placeholder="translate.search"
+      class="input"
+    />
+    <button class="btn" @click="emitSearch">
+      <img src="/src/assets/imgs/search-cmp/search.svg" alt="search button" class="img"/>
+    </button>
+  </section>
+</template>
+
+<script lang="ts">
+import { langService } from "@/services/lang-service";
+
+export default {
+  emits: ["search"],
+  computed: {
+    currentLang() {
+      return langService.currentLang.value;
+    },
+    translate() {
+      return langService.translate[this.currentLang];
+    },
+  },
+  data() {
+    return {
+      searchValue: "",
+    };
+  },
+  methods: {
+    emitSearch() {
+      this.$emit("search", this.searchValue);
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.search {
+  display: flex;
+  width: 400px;
+  border-radius: 32px;
+  border: 1px solid var(--black);
+  overflow: hidden;
+  .input {
+    border: none;
+    background-color: var(--bg);
+    flex: 1;
+    padding-left: 10px;
+    &:focus {
+      outline: none;
+    }
+  }
+  .btn {
+    border: none;
+    padding: 8px 16px;
+    .img{
+        width: 16px;
+        height: 16px;
+    }
+  }
+}
+</style>
