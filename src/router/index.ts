adc9585c5ae81932938977e1a1a4fe45ba14fe60
votex001 <jeneea45@gmail.com>
2025-04-02 19:00:32 +0300

    new routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,24 @@
-import Home from '@/views/Home.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-
-const routes = [
-    { path: '/', component: Home  },
-
-]
+import CustomersSettings from "@/outlets/CustomersSettings.vue";
+import MetalsSettings from "@/outlets/MetalsSettings.vue";
+import Home from "@/views/Home.vue";
+import Settings from "@/views/Settings.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+const routes: RouteRecordRaw[] = [
+  { path: "/", component: Home },
+  { path: "/drawings", component: Home },
+  {
+    path: "/settings",
+    component: Settings,
+    children: [
+      { path: "", component: CustomersSettings },
+      { path: "metals", component: MetalsSettings },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
