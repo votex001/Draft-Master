327442be40783a9b6d52231136ad8a6b5b691279
votex001 <jeneea45@gmail.com>
2025-04-04 11:02:54 +0300

    new component

---
+++ src/components/ColumnHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="column-header" @click="onClick">
+    <p>{{ name }}</p>
+    <img
+      src="/src/assets/imgs/column-header/arrow.svg"
+      alt="arrow"
+      v-if="arrow"
+      class="arrow"
+      :class="{ rotate: arrow === 'up' }"
+    />
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  name: "Column Header",
+  props: {
+    name: { type: String, required: true },
+    arrow: { type: String as PropType<"up" | "down" | "">, required: false },
+  },
+  emits: ["sort"],
+  methods: {
+    onClick() {
+      this.$emit("sort");
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.column-header {
+  display: flex;
+  justify-content: space-between;
+user-select: none;
+  cursor: pointer;
+  padding: 5px 10px;
+  .arrow {
+    width: 24px;
+    height: 24px;
+    transition: transform 0.3s ease-out;
+    &.rotate {
+      transform: rotate(180deg);
+    }
+  }
+}
+</style>
