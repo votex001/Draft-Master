c1484538b0bd94ae97e3a0823bc24c71fe11838c
votex001 <jeneea45@gmail.com>
2025-04-06 21:50:01 +0300

    debug

--- src/services/test.data.ts
+++ src/services/test.data.ts
@@ -1,4 +1,4 @@
-export  const costumers = [
+export const customers = [
     {
       id: 'b7fdf1ec-98b1-4c3c-81ea-3c3ef1d8d6af',
       name: 'Default',
@@ -7,7 +7,9 @@ export  const costumers = [
         'Painted 0.4-1': 1,
         'Galvanized 0.4-1': 1,
         'Galvanized 1-4': 1
-      }
+      },
+      lastOrder: Math.floor(Math.random() * 1000000000000),  // случайное число
+      lastEdit: Math.floor(Math.random() * 1000000000000)  // случайное число
     },
     {
       id: 'f2c4e7a1-6d1e-4b3f-996f-b01329ae7b17',
@@ -16,7 +18,9 @@ export  const costumers = [
         'Painted 0.4-1': 3.2,
         'Galvanized 0.4-1': 7.6,
         'Galvanized 1-4': 4.1
-      }
+      },
+      lastOrder: Math.floor(Math.random() * 1000000000000),
+      lastEdit: Math.floor(Math.random() * 1000000000000)
     },
     {
       id: '7c6a3f49-1181-43c0-bd89-56e79fdca733',
@@ -25,7 +29,9 @@ export  const costumers = [
         'Painted 0.4-1': 9.9,
         'Galvanized 0.4-1': 2.3,
         'Galvanized 1-4': 8.7
-      }
+      },
+      lastOrder: Math.floor(Math.random() * 1000000000000),
+      lastEdit: Math.floor(Math.random() * 1000000000000)
     },
     {
       id: '0a71cbbf-7289-4cd3-972b-e0f8f6a9e5c6',
@@ -34,7 +40,9 @@ export  const costumers = [
         'Painted 0.4-1': 5.4,
         'Galvanized 0.4-1': 1.2,
         'Galvanized 1-4': 6.9
-      }
+      },
+      lastOrder: Math.floor(Math.random() * 1000000000000),
+      lastEdit: Math.floor(Math.random() * 1000000000000)
     },
     {
       id: 'a4c726a9-d0f6-4cc7-a93c-e8b4086e2d45',
@@ -43,7 +51,9 @@ export  const costumers = [
         'Painted 0.4-1': 7.1,
         'Galvanized 0.4-1': 3.8,
         'Galvanized 1-4': 2.6
-      }
+      },
+      lastOrder: Math.floor(Math.random() * 1000000000000),
+      lastEdit: Math.floor(Math.random() * 1000000000000)
     },
     {
       id: '318fb734-b72a-4628-a8b6-ecfd1a8d1b68',
@@ -52,7 +62,9 @@ export  const costumers = [
         'Painted 0.4-1': 0.5,
         'Galvanized 0.4-1': 6.3,
         'Galvanized 1-4': 5.5
-      }
+      },
+      lastOrder: Math.floor(Math.random() * 1000000000000),
+      lastEdit: Math.floor(Math.random() * 1000000000000)
     },
     {
       id: '9d39b3ce-1c8d-46f5-8e96-7e20f10d1906',
@@ -61,7 +73,9 @@ export  const costumers = [
         'Painted 0.4-1': 4.4,
         'Galvanized 0.4-1': 8.8,
         'Galvanized 1-4': 0.9
-      }
+      },
+      lastOrder: Math.floor(Math.random() * 1000000000000),
+      lastEdit: Math.floor(Math.random() * 1000000000000)
     },
     {
       id: '6bda9cfc-463a-49f0-bf4f-032ad2ecdd5f',
@@ -70,7 +84,9 @@ export  const costumers = [
         'Painted 0.4-1': 9.1,
         'Galvanized 0.4-1': 2.2,
         'Galvanized 1-4': 7.3
-      }
+      },
+      lastOrder: Math.floor(Math.random() * 1000000000000),
+      lastEdit: Math.floor(Math.random() * 1000000000000)
     },
     {
       id: 'ab8f1cb2-7325-4e23-9814-b52cc47f60dc',
@@ -79,7 +95,9 @@ export  const costumers = [
         'Painted 0.4-1': 1.9,
         'Galvanized 0.4-1': 9.7,
         'Galvanized 1-4': 3.0
-      }
+      },
+      lastOrder: Math.floor(Math.random() * 1000000000000),
+      lastEdit: Math.floor(Math.random() * 1000000000000)
     },
     {
       id: 'fe308c6a-5c23-4e83-8fcf-08de02a9ec30',
@@ -88,7 +106,9 @@ export  const costumers = [
         'Painted 0.4-1': 6.5,
         'Galvanized 0.4-1': 4.6,
         'Galvanized 1-4': 1.1
-      }
+      },
+      lastOrder: Math.floor(Math.random() * 1000000000000),
+      lastEdit: Math.floor(Math.random() * 1000000000000)
     }
   ];
   
