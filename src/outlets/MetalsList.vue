8312210e077b113550505e866174bd33cd7e4eca
votex001 <jeneea45@gmail.com>
2025-04-13 15:48:42 +0300

    removing double code in MetalList and TypesList
    no if i have like 10 lists i dont need to make same code in all of them
    only 4 lines of hook useItemStoreControls

--- src/outlets/MetalsList.vue
+++ src/outlets/MetalsList.vue
@@ -1,78 +1,56 @@
 <template>
   <section class="metals-list">
-    <section class="table-section">
-      <section class="table">
-        <ColumnHeader name="Name" :arrow="dir as 1|-1" @sort="onSort" />
-        <ItemList
-          :items="metals"
-          display-key="name"
-          @selected="onSelect($event)"
-        />
-      </section>
-      <ActionToolbar
-        @search="onSearch($event)"
-        :btn-names="['Add', 'Edit', 'Delete']"
-        placeholder="Metal"
-        :show-items-actions="!!selectedMetalId"
-        @btn1="onAddNew($event)"
-        @btn2="onEditMetal"
-        @btn3="onDeleteMetal"
-      />
-    </section>
+    <ItemListComponent
+      header-name="Name"
+      placeholder="Metal"
+      :items="metals"
+      display-key="name"
+      @add="logic.onAdd"
+      @edit="onEdit"
+      @delete="logic.onDelete"
+      @query="logic.onQuery"
+      @select="logic.onSelect"
+    />
   </section>
 </template>
 
 <script lang="ts">
-import ActionToolbar from "@/components/ActionToolbar.vue";
-import ColumnHeader from "@/components/ColumnHeader.vue";
-import ItemList from "@/components/ItemList.vue";
-import { Metal } from "@/models/metal.model";
-import { langService } from "@/services/lang-service";
 import { defineComponent } from "vue";
+import ItemListComponent from "@/components/ItemListComponent.vue";
+import { Metal } from "@/models/metal.model";
+import { useItemStoreControls } from "@/services/useItemStoreControls";
+import { langService } from "@/services/lang-service";
+
 export default defineComponent({
+  name: "MetalList",
+  components: { ItemListComponent },
+
   computed: {
     translate() {
       const currentLang = langService.currentLang.value;
       return langService.translate[currentLang];
     },
-    metals(): Metal[] {
+    metals(): Array<Metal & { id: string }> {
       return this.$store.getters.getMetals;
     },
-  },
-  data() {
-    return {
-      selectedMetalId: null as null | string,
-      dir: 1 as 1 | -1,
-    };
-  },
-  methods: {
-    onSelect(item: Metal | null) {
-      this.selectedMetalId = item ? item.id : null;
-    },
-    onSearch(value: string) {
-      console.log(`metal:`, value);
-    },
-    onAddNew(name: string) {
-      console.log("[customersSettings:Add btn]", name);
-    },
-    onEditMetal() {
-      console.log("edit:", this.selectedMetalId);
-    },
-    onDeleteMetal() {
-      console.log("delete:", this.selectedMetalId);
-    },
-    onSort() {
-      this.dir = this.dir === 1 ? -1 : 1;
-    },
-    loadMetals() {
-      this.$store.dispatch("loadMetals");
+    logic() {
+      const logic = useItemStoreControls<Array<Metal & { id: string }>>({
+        loadAction: "loadMetals",
+        saveAction: "saveMetal",
+        deleteAction: "deleteMetal",
+      });
+      return logic;
     },
   },
+
   beforeMount() {
-    this.loadMetals();
+    this.logic.onQuery();
   },
-  components: { ColumnHeader, ItemList, ActionToolbar },
+
+  methods: {
+    onEdit() {
+      console.log("selectedMetalIdForEdit", this.logic.selectedId.value);
+    },
+  },
 });
 </script>
-
-<style scoped lang="scss"></style>
--- src/outlets/TypesList.vue
+++ src/outlets/TypesList.vue
@@ -1,78 +1,56 @@
 <template>
-  <section class="types-list">
-    <section class="table-section">
-      <section class="table">
-        <ColumnHeader name="Name" :arrow="dir as 1|-1" @sort="onSort()" />
-        <ItemList
-          :items="metalTypes"
-          display-key="type"
-          @selected="onSelect($event)"
-        />
-      </section>
-      <ActionToolbar
-        @search="onSearch($event)"
-        :btn-names="['Add', 'Edit', 'Delete']"
-        placeholder="Type"
-        :show-items-actions="!!selectedTypeId"
-        @btn1="onAddNew($event)"
-        @btn2="onEditType"
-        @btn3="onDeleteType"
-      />
-    </section>
+  <section class="metal-types-list">
+    <ItemListComponent
+      header-name="Name"
+      placeholder="Type"
+      :items="metalTypes"
+      display-key="type"
+      @add="logic.onAdd"
+      @edit="onEdit"
+      @delete="logic.onDelete"
+      @query="logic.onQuery"
+      @select="logic.onSelect"
+    />
   </section>
 </template>
 
 <script lang="ts">
-import ActionToolbar from "@/components/ActionToolbar.vue";
-import ColumnHeader from "@/components/ColumnHeader.vue";
-import ItemList from "@/components/ItemList.vue";
-import { MetalType } from "@/models/metal.model";
-import { langService } from "@/services/lang-service";
 import { defineComponent } from "vue";
+import ItemListComponent from "@/components/ItemListComponent.vue";
+import { MetalType } from "@/models/metal.model";
+import { useItemStoreControls } from "@/services/useItemStoreControls";
+import { langService } from "@/services/lang-service";
+
 export default defineComponent({
+  components: { ItemListComponent },
+
   computed: {
     translate() {
       const currentLang = langService.currentLang.value;
       return langService.translate[currentLang];
     },
-    metalTypes() {
+    metalTypes(): Array<MetalType & { id: string }> {
       return this.$store.getters.getMetalTypes;
     },
-  },
-  data() {
-    return {
-      selectedTypeId: null as null | string,
-      dir: 1 as 1 | -1,
-    };
-  },
-  methods: {
-    onSelect(item: MetalType | null) {
-      this.selectedTypeId = item ? item.id : null;
-    },
-    onSearch(value: string) {
-      console.log(`type:`, value);
-    },
-    onAddNew(name: string) {
-      console.log("[customersSettings:Add btn]:", name);
-    },
-    onEditType() {
-      console.log("edit:", this.selectedTypeId);
-    },
-    onDeleteType() {
-      console.log("delete:", this.selectedTypeId);
-    },
-    onSort() {
-      this.dir = this.dir === 1 ? -1 : 1;
-    },
-    loadMetals() {
-      this.$store.dispatch("loadMetalTypes");
+
+    logic() {
+      const logic = useItemStoreControls<Array<MetalType & { id: string }>>({
+        loadAction: "loadMetalTypes",
+        saveAction: "saveMetalType",
+        deleteAction: "deleteMetalType",
+      });
+      return logic;
     },
   },
+
   beforeMount() {
-    this.loadMetals();
+    this.logic.onQuery();
   },
-  components: { ColumnHeader, ItemList, ActionToolbar },
+
+  methods: {
+    onEdit() {
+      console.log("selectedMetalIdForEdit", this.logic.selectedId.value);
+    },
+  },
 });
 </script>
-
-<style scoped lang="scss"></style>
