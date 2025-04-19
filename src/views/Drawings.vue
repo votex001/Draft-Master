a72e905f759bdf6559baa77787d9a32966475baa
votex001 <jeneea45@gmail.com>
2025-04-19 13:01:55 +0300

    Create Drawings.vue

---
+++ src/views/Drawings.vue
@@ -0,0 +1,32 @@
+<template>
+  <HeaderCmp />
+  <section class="drawings">
+    <header class="header">
+      <h1></h1>
+      <p></p>
+    </header>
+    <main>
+      <SearchCmp />
+    </main>
+  </section>
+</template>
+
+<script lang="ts">
+import HeaderCmp from "@/components/header/HeaderCmp.vue";
+import SearchCmp from "@/components/shared/SearchCmp.vue";
+import { langService } from "@/services/lang-service";
+import { defineComponent } from "vue";
+export default defineComponent({
+  computed: {
+    translate() {
+      return langService.translate.value;
+    },
+  },
+  components: {
+    HeaderCmp,
+    SearchCmp,
+  },
+});
+</script>
+
+<style scoped lang="scss"></style>
