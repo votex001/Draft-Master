a2149fc7a1277663c32624c75c29ca5abefb28a3
votex001 <jeneea45@gmail.com>
2025-04-21 21:05:28 +0300

    Create DrawingsTableHeader.vue

---
+++ src/components/table-cmps/drawings-table/DrawingsTableHeader.vue
@@ -0,0 +1,75 @@
+<template>
+  <section class="drawings-table-header">
+    <ColumnHeader
+      v-for="key in Object.keys(arrows)"
+      :key="key"
+      :name="names[key]"
+      :arrow="arrows[key]"
+      @sort="onSort(key as keyof typeof arrows)"
+      class="column-header"
+      :class="key"
+    />
+    <span class="column-header empty"></span>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ColumnHeader from "../ColumnHeader.vue";
+import { langService } from "@/services/lang-service";
+export default defineComponent({
+  components: { ColumnHeader },
+  computed: {
+    names() {
+      const translate = langService.translate.value;
+      return translate.drawingsPage.tables;
+    },
+  },
+  data() {
+    return {
+      arrows: {
+        name: 1 as -1 | 1 | null,
+        metals: null as -1 | 1 | null,
+        totalPrice: null as -1 | 1 | null,
+        lastPrint: null as -1 | 1 | null,
+        lastEdit: null as -1 | 1 | null,
+      },
+    };
+  },
+  methods: {
+    onSort(
+      column: "name" | "lastEdit" | "metals" | "totalPrice" | "lastPrint"
+    ) {
+      Object.keys(this.arrows).forEach((key) => {
+        if (key !== column) this.arrows[key] = null;
+      });
+      this.arrows[column] = this.arrows[column] === 1 ? -1 : 1;
+      this.$emit("sort", { column, dir: this.arrows[column] });
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.drawings-table-header {
+  display: grid;
+  grid-column: 1 / -1;
+  grid-template-columns: subgrid;
+  margin-bottom: 1px;
+  background-color: var(--white);
+  & > * {
+    box-shadow: 0px 4px 4px var(--divider);
+  }
+}
+@media (max-width: 1000px) {
+  .drawings-table-header {
+    & > * {
+      display: none;
+    }
+    & > :first-child,
+    & > :last-child {
+      display: flex; // show only first and last
+    }
+  }
+}
+</style>
