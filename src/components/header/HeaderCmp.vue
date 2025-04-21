410e0c149c7c6c91ddc608bdb0d27868d0d5e858
votex001 <jeneea45@gmail.com>
2025-04-21 19:39:18 +0300

    css direction ltr

--- src/components/header/HeaderCmp.vue
+++ src/components/header/HeaderCmp.vue
@@ -1,107 +1,114 @@
 <template>
-  <header class="header">
-    <router-link to="/" class="nav-link">
-      <picture class="img">
-        <source
-          srcset="/src/assets/imgs/header-cmp/Logo-small.svg"
-          media="(max-width: 480px)"
-        />
-        <img src="/src/assets/imgs/header-cmp/Logo.svg" alt="" /> </picture
-    ></router-link>
-    <section class="btns">
-      <nav class="navigation">
-        <router-link to="/drawings" class="nav-link">{{
-          translate.drawings
-        }}</router-link>
-        <router-link to="/settings" class="nav-link">{{
-          translate.settings
-        }}</router-link>
-      </nav>
-      <LangBtn />
-    </section>
-  </header>
+    <header class="header">
+        <router-link to="/" class="nav-link">
+            <picture class="img">
+                <source
+                    srcset="/src/assets/imgs/header-cmp/Logo-small.svg"
+                    media="(max-width: 480px)"
+                />
+                <img
+                    src="/src/assets/imgs/header-cmp/Logo.svg"
+                    alt=""
+                /> </picture
+        ></router-link>
+        <section class="btns">
+            <nav class="navigation">
+                <router-link to="/drawings" class="nav-link">{{
+                    translate.drawings
+                }}</router-link>
+                <router-link to="/settings" class="nav-link">{{
+                    translate.settings
+                }}</router-link>
+            </nav>
+            <LangBtn />
+        </section>
+    </header>
 </template>
 <script lang="ts">
-import { langService } from "@/services/lang-service";
-import LangBtn from "./LangBtn.vue";
-import { defineComponent } from "vue";
+import { langService } from "@/services/lang-service"
+import LangBtn from "./LangBtn.vue"
+import { defineComponent } from "vue"
 
 export default defineComponent({
-  computed: {
-    translate() {
-      return langService.translate.value;
+    computed: {
+        translate() {
+            return langService.translate.value
+        },
     },
-  },
-  components: {
-    LangBtn,
-  },
-});
+    components: {
+        LangBtn,
+    },
+})
 </script>
 
 <style scoped lang="scss">
 .header {
-  display: flex;
-  justify-content: space-between;
-  max-width: 100%;
-  padding: 0 60px;
-  align-items: center;
-  box-shadow: 0px 1px 1px var(--divider);
-  .btns {
     display: flex;
+    justify-content: space-between;
+    max-width: 100%;
+    padding: 0 60px;
     align-items: center;
-    gap: 30px;
-    .navigation {
-      display: flex;
-      gap: 10px;
-      .nav-link {
-        color: var(--black);
-        text-transform: capitalize;
-        font-weight: bold;
-        font-size: 18px;
-        padding: 10px 5px;
-        &.router-link-active {
-          text-decoration: underline;
-        }
-      }
+    box-shadow: 0px 1px 1px var(--divider);
+    direction: ltr;
+    * {
+        direction: ltr;
     }
-  }
+    .btns {
+        display: flex;
+        align-items: center;
+        gap: 30px;
+        .navigation {
+            display: flex;
+            gap: 10px;
+            .nav-link {
+                color: var(--black);
+                text-transform: capitalize;
+                font-weight: bold;
+                font-size: 18px;
+                padding: 10px 5px;
+                &.router-link-active {
+                    text-decoration: underline;
+                }
+            }
+        }
+    }
 }
 @media (max-width: 768px) {
-  .header {
-    .img {
-      margin-right: 10px;
+    .header {
+        .img {
+            margin-right: 10px;
+        }
+        .btns {
+            .navigation {
+                gap: 15px;
+            }
+        }
     }
-    .btns {
-      .navigation {
-        gap: 15px;
-      }
-    }
-  }
 }
 @media (max-width: 480px) {
-  .header {
-    padding: 0 30px;
-    .btns {
-      gap: 5px;
-      .navigation {
-        gap: 0;
-        .nav-link {
-          font-size: 14px;
+    .header {
+        padding: 0 30px;
+        .btns {
+            gap: 5px;
+            .navigation {
+                gap: 0;
+                .nav-link {
+                    font-size: 14px;
+                }
+            }
         }
-      }
     }
-  }
 }
 @media (max-width: 360px) {
-  .header {
-    padding: 0 10px;
-    .btns {
-      .navigation {
-        .nav-link {
-          font-size: 12px;
+    .header {
+        padding: 0 10px;
+        .btns {
+            .navigation {
+                .nav-link {
+                    font-size: 12px;
+                }
+            }
         }
-      }
     }
-  }
 }
 </style>
