71f47cfdf1785367c8cd33dfe55b422f9f93e519
votex001 <jeneea45@gmail.com>
2025-06-27 01:42:36 +0300

    took out DraftMetal

--- src/models/drafts.model.ts
+++ src/models/drafts.model.ts
@@ -1,17 +1,19 @@
 export interface Draft {
   customerName: string;
   customerId: string;
-  metals: {
-    name: string;
-    type: string;
-    paymentPerBending: number;
-    metalThickness: number;
-    metalPrice: number;
-    amount: number;
-    width: number;
-    price: number;
-    deployment: number;
-    bendings: { bendingNum: number; width: number }[];
-  }[];
+  metals: DraftMetal[];
   totalPrice: number;
+}
+
+export interface DraftMetal {
+  name: string;
+  type: string;
+  paymentPerBending: number;
+  metalThickness: number;
+  metalPrice: number;
+  amount: number;
+  width: number;
+  price: number;
+  deployment: number;
+  bendings: { bendingNum: number; width: number }[];
 }
