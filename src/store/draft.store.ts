df10958e9e02100f8fc7b1dc91d41faaad5dd273
votex001 <jeneea45@gmail.com>
2025-06-29 23:40:42 +0300

    changing default new draft

--- src/store/draft.store.ts
+++ src/store/draft.store.ts
@@ -21,6 +21,7 @@ export const draftStore = {
         localStorage.removeItem("draft");
         return null;
       }
+      // console.log(customer);
       const newDraft: Draft = {
         customerId: customer.id,
         customerName: customer.name,
@@ -28,16 +29,34 @@ export const draftStore = {
           {
             amount: 6,
             bendings: [],
-            deployment: 0.33,
+            deployment: 0,
             metalPrice: customer.prices["Default"] | 1,
             name: "Default",
             paymentPerBending: customer.prices["Bending price"] | 1,
-            price: 0,
+            price: customer.prices["Default"] | 1,
             type: "Default",
             width: 3,
-            bendingFee: true ,
+            bendingFee: true,
             metalThickness: 0.5,
           },
+          // {
+          //   amount: 15,
+          //   bendings: [
+          //     { width: 30, bendingNum: 1 },
+          //     { width: 50, bendingNum: 1 },
+          //     { width: 40, bendingNum: 1 },
+          //     { width: 30, bendingNum: 1 },
+          //   ],
+          //   deployment: 0.15,
+          //   metalPrice: 8,
+          //   name: "Default",
+          //   paymentPerBending: 1.5,
+          //   price: 8,
+          //   type: "Default",
+          //   width: 4,
+          //   bendingFee: true,
+          //   metalThickness: 0.6,
+          // },
         ],
         totalPrice: 0,
       };
@@ -55,6 +74,12 @@ export const draftStore = {
           console.error("Error get draft", e);
         }
       }
+    },
+    updateDraft({ state, commit }, updateFields: Partial<Draft>) {
+      if (!state.currentDraft) return;
+      const updatedDraft = { ...state.currentDraft, ...updateFields };
+      commit("setCurrentDraft", updatedDraft);
+      localStorage.setItem("draft", JSON.stringify(updatedDraft));
     },
   },
   getters: {
