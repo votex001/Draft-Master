59881ad49d2f2426243c5be037e2a4064fcda6ad
votex001 <jeneea45@gmail.com>
2025-06-29 23:38:01 +0300

    auto update totalPrice

--- src/views/Draft.vue
+++ src/views/Draft.vue
@@ -3,7 +3,7 @@
   <main>
     <DraftHeader :title="draft.customerName" />
     <main>{{ draft }}</main>
-    <DraftFooter :metals="draft.metals" :total-price="draft.totalPrice"/>
+    <DraftFooter :metals="draft.metals" :total-price="draft.totalPrice" />
   </main>
 </template>
 
@@ -11,6 +11,7 @@
 import DraftFooter from "@/components/footers/DraftFooter.vue";
 import DraftHeader from "@/components/headers/DraftHeader.vue";
 import HeaderCmp from "@/components/headers/HeaderCmp.vue";
+import { metalService } from "@/services/metal.service";
 import { defineComponent } from "vue";
 
 export default defineComponent({
@@ -35,6 +36,19 @@ export default defineComponent({
       if (newVal === null) {
         this.$router.push("/drafts");
       }
+    },
+    "draft.metals": {
+      handler(newMetals) {
+        const calculatedTotalPrice =
+          metalService.getTotalPriceFromMetals(newMetals);
+        if (this.draft.totalPrice !== calculatedTotalPrice) {
+          this.$store.dispatch("updateDraft", {
+            totalPrice: calculatedTotalPrice,
+          });
+        }
+      },
+      deep: true,
+      immediate: true,
     },
   },
   async beforeCreate() {
