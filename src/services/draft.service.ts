aa4365020a2f5eea88cd38a651ecdb97c5d1dcb2
votex001 <jeneea45@gmail.com>
2025-07-09 19:18:37 +0300

    refactor: extract reusable logic

--- src/services/draft.service.ts
+++ src/services/draft.service.ts
@@ -1,8 +1,14 @@
 import { Bending } from "@/models/drafts.model";
+import { makeId } from "./service";
+import { Customer } from "@/models/custumer.model";
+import { metalService } from "./metal.service";
+import { metalTypesService } from "./metal.types.service";
+import { Metal, MetalType } from "@/models/metal.model";
 
 export const draftService = {
   getMetalSummary,
   getTotalPriceFromMetals,
+  createNewDraftMetal,
 };
 
 interface metalStats {
@@ -54,4 +60,33 @@ function getTotalPriceFromMetals(metals: metalStats[]): number {
   }, 0);
 
   return parseFloat(total.toFixed(2));
+}
+
+async function createNewDraftMetal(customer: Customer) {
+  const defaultMetal: Metal = (
+    await metalService.getQuery({
+      name: "default",
+    })
+  )[0];
+  const defaultType: MetalType = (
+    await metalTypesService.getQuery({
+      type: "default",
+    })
+  )[0];
+  return {
+    id: makeId(),
+    amount: 1,
+    bendings: [],
+    deployment: 0,
+    metalPrice: customer.prices["Default"] | 1,
+    name: defaultMetal.name,
+    metalId: defaultMetal.id,
+    paymentPerBending: customer.prices["Bending price"] | 1,
+    price: customer.prices["Default"] | 1,
+    type: defaultType.type,
+    typeId: defaultType.id,
+    width: 3,
+    bendingFee: true,
+    metalThickness: 0.5,
+  };
 }
--- src/store/draft.store.ts
+++ src/store/draft.store.ts
@@ -1,9 +1,6 @@
 import { Customer } from "@/models/custumer.model";
 import { Draft } from "@/models/drafts.model";
-import { Metal, MetalType } from "@/models/metal.model";
-import { metalService } from "@/services/metal.service";
-import { metalTypesService } from "@/services/metal.types.service";
-import { makeId } from "@/services/service";
+import { draftService } from "@/services/draft.service";
 
 export const draftStore = {
   state(): {
@@ -25,56 +22,12 @@ export const draftStore = {
         localStorage.removeItem("draft");
         return null;
       }
-      const defaultMetal: Metal = (
-        await metalService.getQuery({
-          name: "default",
-        })
-      )[0];
-      const defaultType: MetalType = (
-        await metalTypesService.getQuery({
-          type: "default",
-        })
-      )[0];
-      // console.log(customer);
+      const emptyDraftMetal = await draftService.createNewDraftMetal(customer);
+
       const newDraft: Draft = {
         customerId: customer.id,
         customerName: customer.name,
-        metals: [
-          {
-            id: makeId(),
-            amount: 1,
-            bendings: [],
-            deployment: 0,
-            metalPrice: customer.prices["Default"] | 1,
-            name: defaultMetal.name,
-            metalId: defaultMetal.id,
-            paymentPerBending: customer.prices["Bending price"] | 1,
-            price: customer.prices["Default"] | 1,
-            type: defaultType.type,
-            typeId: defaultType.id,
-            width: 3,
-            bendingFee: true,
-            metalThickness: 0.5,
-          },
-          // {
-          //   amount: 15,
-          //   bendings: [
-          //     { width: 30, bendingNum: 1 },
-          //     { width: 50, bendingNum: 1 },
-          //     { width: 40, bendingNum: 1 },
-          //     { width: 30, bendingNum: 1 },
-          //   ],
-          //   deployment: 0.15,
-          //   metalPrice: 8,
-          //   name: "Default",
-          //   paymentPerBending: 1.5,
-          //   price: 8,
-          //   type: "Default",
-          //   width: 4,
-          //   bendingFee: true,
-          //   metalThickness: 0.6,
-          // },
-        ],
+        metals: [emptyDraftMetal],
         totalPrice: 0,
       };
       commit("setCurrentDraft", newDraft);
