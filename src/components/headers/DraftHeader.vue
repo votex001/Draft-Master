a15627b4f4e95268a73b899de1662d8b2d4e1765
votex001 <jeneea45@gmail.com>
2025-07-11 21:11:00 +0300

    go back button is ready

--- src/components/headers/DraftHeader.vue
+++ src/components/headers/DraftHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="draft-header">
-    <button class="btn" :class="{ loading: isLoading }">
+    <button class="btn" :class="{ loading: isLoading }" @click="isOpen = true">
       <img class="img" src="/src/assets/imgs/draft-header/arrow-left.svg" />Go
       Back
     </button>
@@ -16,19 +16,36 @@
       <button class="btn save" :class="{ loading: isLoading }">Save</button>
       <button class="btn" :class="{ loading: isLoading }">Print</button>
     </section>
+    <Modal v-if="isOpen" @close="isOpen = false">
+      <section class="confirm">
+        <h2 class="title">Do you want to quit without saving?</h2>
+        <section class="btns">
+          <button class="btn white" @click="isOpen = false">Cancel</button>
+          <router-link to="/drafts" class="btn">ok</router-link>
+        </section>
+      </section>
+    </Modal>
   </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Modal from "../modals/Modal.vue";
 export default defineComponent({
   emits: ["add"],
+  data() {
+    return {
+      isOpen: false, //Opening "Go back" confirm window
+    };
+  },
+  components: { Modal },
   props: {
     title: { type: String, required: true },
     isLoading: { type: Boolean, default: false },
   },
 });
 </script>
+
 <style scoped lang="scss">
 .draft-header {
   width: 100%;
@@ -74,6 +91,18 @@ export default defineComponent({
       &:hover {
         background-color: var(--select-hover);
       }
+    }
+  }
+  .confirm {
+    padding: 0 30px 30px 30px;
+    .title {
+      font-size: 18px;
+    }
+    .btns {
+      display: flex;
+      justify-content: end;
+      margin-top: 30px;
+      gap: 20px;
     }
   }
 }
