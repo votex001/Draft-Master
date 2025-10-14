0ccb3c0b9647857d5972f5d079b5bff97c36ab3a
votex001 <jeneea45@gmail.com>
2025-10-14 12:47:14 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
-  server: { open: true },
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
 });
